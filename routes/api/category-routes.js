980076f76dbfa3e8dd3543c1f64a91d6137fe685
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-06-28 15:27:43 -0400

    Updated category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -33,6 +33,7 @@ router.get('/:id', (req, res) => {
       id: req.params.id
     },
     include: {
+      model: Product,
       attributes: ['id','product_name','price','stock','category_id']
     },
   })
